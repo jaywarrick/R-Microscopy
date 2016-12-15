be27114e09b343eeefb234de17fe2ab7bcc8ce78
jaywarrick <jay.w.warrick@gmail.com>
2016-12-14 20:59:22 -0600

    Convert Nd2 metadata

---
+++ ConvertNd2Metadata.R
@@ -0,0 +1,10 @@
+# Name starts at 2
+# ExW ... Power starts at 1
+# Exposure starts at 2
+
+duh <- read.table(fileTable$Value[1], sep="~", comment.char = '~')
+duh2 <- data.frame(duh[multi.mixedorder(duh),])
+
+duh2[grep('Name #', duh2$duh.multi.mixedorder.duh...., fixed=T),]
+
+duh2[grep('Exposure #', duh2$duh.multi.mixedorder.duh...., fixed=T),]
